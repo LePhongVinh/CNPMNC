f3341a895594864e10ee9afead9abf32f99d7914
LePhongVinh <125270083+LePhongVinh@users.noreply.github.com>
2023-04-09 19:26:47 +0700

    XongPhanQuyenAdmin

--- LVCMOBILE/Areas/Admin/Controllers/ProductsController.cs
+++ LVCMOBILE/Areas/Admin/Controllers/ProductsController.cs
@@ -10,6 +10,7 @@ using System.Web.Mvc;
 
 namespace LVCMOBILE.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin,Employee")]
     public class ProductsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
